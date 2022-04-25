ec533eb2115288a3f851d8a01a83f4ad79e69667
cesartidrick <cesartidrick@gmail.com>
2022-04-25 13:36:05 -0600

    commit tree e602a62f6e102739266fd3fbf97be67626a23ba1

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // code your solution here
+function saturdayFun(activity ='roller-skate') {
+    if (activity === 'roller-skate') {
+        return `This Saturday, I want to ${activity}!`
+    } else {
+        return `This Saturday, I want to ${activity}!`
+    }
+}
+
+function mondayWork(param1 = 'go to the office') {
+    if (param1 === 'go to the office') {
+        return `This Monday, I will ${param1}.`
+    } else {
+        return `This Monday, I will ${param1}.`
+    }
+}
+
+function wrapAdjective(outerParam1 = '*'){
+    return function (innerParam1 = 'special') {
+        return `You are ${outerParam1}${innerParam1}${outerParam1}!`
+    }
+}
